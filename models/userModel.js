9c0f855b708ba01c97bc8204991d9841e03eb18d
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2023-12-16 13:32:43 +0530

    add vaildataor

--- models/userModel.js
+++ models/userModel.js
@@ -1,14 +1,30 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Invalid email address",
+    },
+  },
   companyname: { type: String },
   companyid: { type: String },
-  contact: { type: String },
+  contact: {
+    type: String,
+    validate: {
+      validator: value => validator.isMobilePhone(value, "any", { strictMode: false }),
+      message: "Invalid contact number",
+    },
+  },
   password: { type: String, required: true },
   resetToken: { type: String, required: false },
 });
 
 const userModel = mongoose.model("User", userSchema);
+
 export default userModel;
