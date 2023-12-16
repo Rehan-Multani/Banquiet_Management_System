8c37cf6e4788e05a17cec8f0bbfd7d3f2ae7c3e9
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2023-12-16 13:45:42 +0530

    role added
    role added

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    companyname: { type: String },
-    companyid: { type: String},
-    contact:{type: String},
-    password: { type: String, required: true },
-    resetToken:{type:String,required:false},
-})
-
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  companyname: { type: String },
+  companyid: { type: String },
+  contact: { type: String },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "waiter", "staff"],
+  },
+  password: { type: String, required: true },
+  resetToken: { type: String, required: false },
+});
 
 const userModel = mongoose.model("User", userSchema);
 export default userModel;
