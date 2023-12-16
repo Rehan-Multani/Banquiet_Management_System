b4e2c96fb4f46daf8091637a49898a59ddb5d6e5
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2023-12-16 13:29:26 +0530

    some changes
    some changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    companyname: { type: String },
-    companyid: { type: String},
-    contact:{type: String},
-    password: { type: String, required: true },
-    resetToken:{type:String,required:false},
-})
-
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  companyname: { type: String },
+  companyid: { type: String },
+  contact: { type: String },
+  password: { type: String, required: true },
+  resetToken: { type: String, required: false },
+});
 
 const userModel = mongoose.model("User", userSchema);
 export default userModel;
