ae20cebaa7e5b2ee0967b61b6dddad5251a79ff9
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2023-12-16 13:33:36 +0530

    remove vaildator

--- models/userModel.js
+++ models/userModel.js
@@ -1,30 +1,15 @@
 import mongoose from "mongoose";
-import validator from "validator";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: "Invalid email address",
-    },
-  },
-  companyname: { type: String },
-  companyid: { type: String },
-  contact: {
-    type: String,
-    validate: {
-      validator: value => validator.isMobilePhone(value, "any", { strictMode: false }),
-      message: "Invalid contact number",
-    },
-  },
-  password: { type: String, required: true },
-  resetToken: { type: String, required: false },
-});
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    companyname: { type: String },
+    companyid: { type: String},
+    contact:{type: String},
+    password: { type: String, required: true },
+    resetToken:{type:String,required:false},
+})
+
 
 const userModel = mongoose.model("User", userSchema);
-
 export default userModel;
