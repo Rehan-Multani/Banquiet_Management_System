5f3136365b1a4253530e31ff34719691c3c90c9d
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2023-12-23 11:00:28 +0530

    new delete notification route and create and update booking
    new delete notification route and create and update booking

--- controllers/adminnotificationController/adminnotificationController.js
+++ controllers/adminnotificationController/adminnotificationController.js
@@ -20,5 +20,36 @@ const createadminnotification = async (req, res) => {
       .send({ success: false, message: "internal server Error" + error });
   }
 };
+const deleteadminnotification = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const notificationToDelete = await adminNotification.findByIdAndDelete(
+        req.params.id
+      );
 
-export { createadminnotification };
+      if (notificationToDelete) {
+        res.status(200).send({
+          success: true,
+          notificationToDelete,
+        });
+      } else {
+        res.status(404).send({
+          success: false,
+          message: "Notification not found",
+        });
+      }
+    } else {
+      res.status(400).send({
+        success: false,
+        message: "ID is not available for deleting notification",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Internal server error: " + error.message,
+    });
+  }
+};
+
+export { createadminnotification, deleteadminnotification };
