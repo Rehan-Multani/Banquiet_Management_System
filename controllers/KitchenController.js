63d0afd88579e3b102878faa95825f43187ebcbe
Hariom Bhati <122555359+hariombhati@users.noreply.github.com>
2024-01-12 17:47:56 +0530

    Update KitchenController.js

--- controllers/KitchenController.js
+++ controllers/KitchenController.js
@@ -5,25 +5,23 @@ const updateKitchen = async (req, res) => {
 
   try {
     const updatedMenuPromises = items.map(async (item) => {
-      // Check if the item is available in req.body
+    
       if (item._id) {
-        // Fetch the existing item without updating to retain the old data
+       
         const existingItem = await menuModel.findOne({
           _id: id,
           "items._id": item._id,
         });
 
         if (!existingItem) {
-          return null; // If item not found, return null
+          return null; 
         }
 
-        // Update only the fields that are present in req.body
         const updatedFields = {
           "items.$.rating": item.rating || existingItem.items[0].rating,
           "items.$.quantity": item.quantity || existingItem.items[0].quantity,
           "items.$.weight": item.weight || existingItem.items[0].weight,
-          "items.$.kitchenid":
-            item.kitchenid || existingItem.items[0].kitchenid,
+          "items.$.kitchenid": item.kitchenid || existingItem.items[0].kitchenid,
           "items.$.last_update_kitchen": new Date().toString(),
         };
 
@@ -35,7 +33,7 @@ const updateKitchen = async (req, res) => {
 
         return updatedMenu;
       } else {
-        return null; // If item._id is not present, return null
+        return null; 
       }
     });
 
